cc1eb7029d70a443012a9369693f1aaa631a05f1
Manuel <113391900+Manullet@users.noreply.github.com>
2023-10-09 09:48:09 -0600

    detalles en objetos

--- php/eliminar_obj_be.php
+++ php/eliminar_obj_be.php
@@ -4,5 +4,11 @@
     include ("conexion_be.php");
     $sql="delete from objetos where Id_objetos='".$Id_objetos."'";
     $resultado=mysqli_query($conexion, $sql);
+    echo "
+                <script language='JavaScript'>
+                    alert('Datos Actualizados');
+                    location.assign('../index.php');
+                </script>
+            ";
 
 ?>
