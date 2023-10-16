69d6cf66dabe1c0d84f822a516e522b595a197e3
Manuel <113391900+Manullet@users.noreply.github.com>
2023-10-15 18:44:19 -0600

    ajuste para el diseño

--- php/cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-//cierra la sesion iniciada
-session_destroy();
-//redirigue al login
-header("location: ../index.php");
-
-?>
--- php/eliminar_mant_permisos.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $Id_Permisos=$_GET['Id_Permisos'];
-    include ("conexion_be.php");
-    $sql="delete from permisos where Id_Permisos='".$Id_Permisos."'";
-    $resultado=mysqli_query($conexion,$sql);
-
-?>
